6942f22143587e52a73ffc8fd4570e213d02876d
Ed Ball <ed.ball@faithlife.com>
2019-04-25 15:56:18 -0700

    Escape empty app arguments.

--- src/Faithlife.Build/AppRunner.cs
+++ src/Faithlife.Build/AppRunner.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using McMaster.Extensions.CommandLineUtils;
 using SimpleExec;
 
@@ -26,7 +27,7 @@ namespace Faithlife.Build
 		/// <param name="path">The path of the command-line app.</param>
 		/// <param name="args">The arguments to send to the command-line app.</param>
 		public static void RunApp(string path, IEnumerable<string> args) =>
-			Command.Run(path, ArgumentEscaper.EscapeAndConcatenate(args.Where(x => x != null)));
+			Command.Run(path, EscapeAndConcatenate(args));
 
 		/// <summary>
 		/// Runs the specified .NET Framework command-line app.
@@ -52,5 +53,80 @@ namespace Faithlife.Build
 
 			RunApp(path, args);
 		}
+
+		// copied from https://github.com/natemcmaster/CommandLineUtils/blob/master/src/CommandLineUtils/Utilities/ArgumentEscaper.cs
+		// but also ignores null, escapes empty
+		private static string EscapeAndConcatenate(IEnumerable<string> args)
+			=> string.Join(" ", args.Where(x => x != null).Select(EscapeSingleArg));
+
+		private static string EscapeSingleArg(string arg)
+		{
+			var sb = new StringBuilder();
+
+			var needsQuotes = arg.Length == 0 || ContainsWhitespace(arg);
+			var isQuoted = needsQuotes || IsSurroundedWithQuotes(arg);
+
+			if (needsQuotes)
+			{
+				sb.Append('"');
+			}
+
+			for (int i = 0; i < arg.Length; ++i)
+			{
+				var backslashes = 0;
+
+				// Consume all backslashes
+				while (i < arg.Length && arg[i] == '\\')
+				{
+					backslashes++;
+					i++;
+				}
+
+				if (i == arg.Length && isQuoted)
+				{
+					// Escape any backslashes at the end of the arg when the argument is also quoted.
+					// This ensures the outside quote is interpreted as an argument delimiter
+					sb.Append('\\', 2 * backslashes);
+				}
+				else if (i == arg.Length)
+				{
+					// At then end of the arg, which isn't quoted,
+					// just add the backslashes, no need to escape
+					sb.Append('\\', backslashes);
+				}
+				else if (arg[i] == '"')
+				{
+					// Escape any preceding backslashes and the quote
+					sb.Append('\\', (2 * backslashes) + 1);
+					sb.Append('"');
+				}
+				else
+				{
+					// Output any consumed backslashes and the character
+					sb.Append('\\', backslashes);
+					sb.Append(arg[i]);
+				}
+			}
+
+			if (needsQuotes)
+			{
+				sb.Append('"');
+			}
+
+			return sb.ToString();
+		}
+
+		private static bool IsSurroundedWithQuotes(string argument)
+		{
+			if (argument.Length <= 1)
+			{
+				return false;
+			}
+
+			return argument[0] == '"' && argument[argument.Length - 1] == '"';
+		}
+
+		private static bool ContainsWhitespace(string argument)
+			=> argument.IndexOfAny(new[] { ' ', '\t', '\n' }) >= 0;
 	}
 }
