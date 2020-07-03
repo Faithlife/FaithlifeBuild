8e16c82e626a3296e5e21b4d6027d31582ce9315
Ed Ball <ed.ball@faithlife.com>
2020-07-03 13:03:50 -0700

    Update Faithlife.Build.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -14,14 +14,7 @@ internal static class Build
 					GitLogin = new GitLoginInfo("faithlifebuildbot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
 					GitAuthor = new GitAuthorInfo("Faithlife Build Bot", "faithlifebuildbot@users.noreply.github.com"),
 					SourceCodeUrl = "https://github.com/Faithlife/FaithlifeBuild/tree/master/src",
-					GitBranchName = GetGitBranchName(),
 				},
 			});
 	});
-
-	private static string? GetGitBranchName()
-	{
-		const string prefix = "refs/heads/";
-		return Environment.GetEnvironmentVariable("GITHUB_REF") is string githubRef && githubRef.StartsWith("refs/heads/", StringComparison.Ordinal) ? githubRef.Substring(prefix.Length) : null;
-	}
 }
