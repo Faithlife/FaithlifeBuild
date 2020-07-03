33f7a6daee5f3b2efb3d8d286d8281f2f6f232e6
Ed Ball <ed.ball@faithlife.com>
2020-07-03 07:28:25 -0700

    Minimal build verbosity.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -8,6 +8,7 @@ internal static class Build
 		build.AddDotNetTargets(
 			new DotNetBuildSettings
 			{
+				Verbosity = DotNetBuildVerbosity.Minimal,
 				NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
 				DocsSettings = new DotNetDocsSettings
 				{
