01ede176b58593e0066e674913317c59ca931b33
Ed Ball <ed.ball@faithlife.com>
2020-12-17 08:36:11 -0800

    Use slashes instead of backslashes.

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 Push-Location $PSScriptRoot
-dotnet publish tools\Build\Build.csproj --output tools\bin\Build --nologo --verbosity quiet
-if ($LASTEXITCODE -eq 0) { dotnet tools\bin\Build\Build.dll $args }
+dotnet publish ./tools/Build/Build.csproj --output ./tools/bin/Build --nologo --verbosity quiet
+if ($LASTEXITCODE -eq 0) { dotnet ./tools/bin/Build/Build.dll $args }
 Pop-Location
 exit $LASTEXITCODE
