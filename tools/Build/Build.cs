739099aab7bb5cab3ea3f58f8463dc26b52c655d
Ed Ball <ed.ball@faithlife.com>
2021-03-11 19:15:04 -0800

    Test beta release.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -5,9 +5,10 @@ return BuildRunner.Execute(args, build => build.AddDotNetTargets(
 	new DotNetBuildSettings
 	{
 		NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
+		GitLogin = new GitLoginInfo("faithlifebuildbot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
+		PackageSettings = new DotNetPackageSettings { PushTagOnPublish = x => $"v{x.Version}" },
 		DocsSettings = new DotNetDocsSettings
 		{
-			GitLogin = new GitLoginInfo("faithlifebuildbot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
 			GitAuthor = new GitAuthorInfo("Faithlife Build Bot", "faithlifebuildbot@users.noreply.github.com"),
 			SourceCodeUrl = "https://github.com/Faithlife/FaithlifeBuild/tree/master/src",
 		},
