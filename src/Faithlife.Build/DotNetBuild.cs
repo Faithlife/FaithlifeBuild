Faithlife/FaithlifeBuild

commit d90f5423c7480954ee79cf2d35f13eb4283c5966

Use C# 11.

Directory.Build.props
@@ -2,7 +2,7 @@
 
   <PropertyGroup>
     <VersionPrefix>5.15.0</VersionPrefix>
-    <LangVersion>10.0</LangVersion>
+    <LangVersion>11.0</LangVersion>
     <Nullable>enable</Nullable>
     <ImplicitUsings>enable</ImplicitUsings>
     <TreatWarningsAsErrors>true</TreatWarningsAsErrors>

src/Faithlife.Build/DotNetBuild.cs
@@ -572,7 +572,7 @@ public static class DotNetBuild
 				}
 				else
 				{
-					Console.WriteLine("To publish to NuGet, push this tag: v" + GetPackageInfo(packagePaths[0]).Version);
+					Console.WriteLine($"To publish to NuGet, push this tag: v{GetPackageInfo(packagePaths[0]).Version}");
 				}
 			});
 
@@ -722,7 +722,7 @@ public static class DotNetBuild
 					Console.WriteLine($"Pushing git tag {tagToPush} at {commitSha} (using GitHub API).");
 					var request = new HttpRequestMessage(HttpMethod.Post, apiUrl)
 					{
-						Content = new StringContent($"{{\"ref\":\"refs/tags/{tagToPush}\",\"sha\":\"{commitSha}\"}}", Encoding.UTF8, "application/json"),
+						Content = new StringContent($$"""{"ref":"refs/tags/{{tagToPush}}","sha":"{{commitSha}}"}""", Encoding.UTF8, "application/json"),
 					};
 					response = httpClient.Send(request);
 					if (response.StatusCode != HttpStatusCode.Created)
@@ -862,22 +862,16 @@ public static class DotNetBuild
 	/// Gets the argument that specifies the verbosity.
 	/// </summary>
 	/// <remarks>Defaults to minimal.</remarks>
-	public static string GetVerbosityArg(this DotNetBuildSettings settings)
-	{
-		var verbosity = settings.GetVerbosity();
-
-		var argument = verbosity switch
+	public static string GetVerbosityArg(this DotNetBuildSettings settings) =>
+		$"-v{settings.GetVerbosity() switch
 		{
 			DotNetBuildVerbosity.Quiet => "quiet",
 			DotNetBuildVerbosity.Minimal => "minimal",
 			DotNetBuildVerbosity.Normal => "normal",
 			DotNetBuildVerbosity.Detailed => "detailed",
 			DotNetBuildVerbosity.Diagnostic => "diagnostic",
-			_ => throw new BuildException($"Unexpected DotNetBuildVerbosity: {verbosity}"),
-		};
-
-		return $"-v:{argument}";
-	}
+			{ } other => throw new BuildException($"Unexpected DotNetBuildVerbosity: {other}"),
+		}}";
 
 	/// <summary>
 	/// Gets the build number, if any.
@@ -907,8 +901,7 @@ public static class DotNetBuild
 	/// </summary>
 	public static IEnumerable<string> GetExtraPropertyArgs(this DotNetBuildSettings settings, string target)
 	{
-		var pairs = settings.ExtraProperties?.Invoke(target);
-		if (pairs is not null)
+		if (settings.ExtraProperties?.Invoke(target) is { } pairs)
 		{
 			foreach (var (key, value) in pairs)
 				yield return $"-p:{key}={value}";

.github/workflows/build.yaml
@@ -28,10 +28,10 @@ jobs:
     steps:
     - name: Check out code
       uses: actions/checkout@v3
-    - name: Set up .NET 6
+    - name: Set up .NET 7
       uses: actions/setup-dotnet@v3
       with:
-        dotnet-version: 6.0.x
+        dotnet-version: 7.0.x
     - name: Restore
       run: .\build.ps1 restore
     - name: Build

src/Faithlife.Build/AppRunner.cs
@@ -113,7 +113,7 @@ public static class AppRunner
 		if (settings.UseCmdOnWindows && BuildEnvironment.IsWindows())
 		{
 			commandPath = "cmd.exe";
-			argsString = "/S /C \"" + ArgumentEscaper.EscapeAndConcatenate(arguments.Prepend(path)) + "\"";
+			argsString = $"/S /C \"{ArgumentEscaper.EscapeAndConcatenate(arguments.Prepend(path))}\"";
 		}
 		else if (settings.IsFrameworkApp && BuildEnvironment.IsUnix())
 		{

commit db91ca823c1e97229f14ff50bf1ba0874948a010

Fix syntax error in generated verbosity parameter.
This fixes an error introduced in d90f5423c7480954ee79cf2d35f13eb4283c5966.

Directory.Build.props
@@ -1,7 +1,7 @@
 <Project>
 
   <PropertyGroup>
-    <VersionPrefix>5.16.0</VersionPrefix>
+    <VersionPrefix>5.16.1</VersionPrefix>
     <LangVersion>11.0</LangVersion>
     <Nullable>enable</Nullable>
     <ImplicitUsings>enable</ImplicitUsings>

src/Faithlife.Build/DotNetBuild.cs
@@ -897,7 +897,7 @@ public static class DotNetBuild
 	/// </summary>
 	/// <remarks>Defaults to minimal.</remarks>
 	public static string GetVerbosityArg(this DotNetBuildSettings settings) =>
-		$"-v{settings.GetVerbosity() switch
+		$"-v:{settings.GetVerbosity() switch
 		{
 			DotNetBuildVerbosity.Quiet => "quiet",
 			DotNetBuildVerbosity.Minimal => "minimal",